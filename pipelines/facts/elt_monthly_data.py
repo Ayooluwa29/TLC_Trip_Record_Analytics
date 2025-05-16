55fb326606d891af8ca06bc2569dbe62ea746cf8
ajesuniyi <ajesuniyi@aiicoplc.com>
2025-05-16 07:53:39 +0100

    created three files and two pipelines to extract data from parquet files, create a df with it and then load into a table in my database Ayooluwa

---
+++ pipelines/facts/elt_monthly_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+from sqlalchemy import create_engine, engine
+#Configure Postgres Destination
+psql_conn = create_engine('postgresql+psycopg2://postgres:123password@localhost:5432/tlc_trip')
+files_folder = r"C:\Users\ajesuniyi\Documents\TLC_Trip"
+
+
+for filename in os.listdir(files_folder):
+    if filename.endswith('.parquet'):
+        file_path = os.path.join(files_folder, filename)
+        print(f"Loading {file_path}...")
+
+        # Read Parquet file
+        df = pd.read_parquet(file_path)
+
+        # Write to Postgres
+        df.to_sql(name="tlc_trip_yellow_taxi_2022", con=psql_conn, if_exists='replace', index=False)
+
+        print(f"Inserted {len(df)} rows from {filename}")
---
+++ pipelines/facts/elt_monthly_data_v1.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv, find_dotenv
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+DW_CONN = os.environ.get("psql_conn")
+DW_ENGINE = create_engine(DW_CONN)
+files_folder = r"C:\Users\ajesuniyi\Documents\TLC_Trip"
+TARGET_SCHEMA = "bronze"
+table_name = "tlc_trip_yellow_taxi_2022"
+table_name_with_schema = f"{TARGET_SCHEMA}.{table_name}"
+
+
+dtype_mapping = {
+    'object': 'TEXT',
+    'int64': 'BIGINT',
+    'float64': 'NUMERIC',
+    'datetime64[ns]': 'DATE',
+    'bool': 'BOOLEAN',
+    'category': 'TEXT'
+}
+
+with DW_ENGINE.begin() as conn:
+    first_file_processed = False
+    for filename in os.listdir(files_folder):
+        if filename.endswith('.parquet'):
+            file_path = os.path.join(files_folder, filename)
+            print(f"Processing file: {filename}")
+            df = pd.read_parquet(file_path)
+
+            if not first_file_processed:
+                columns_definition = []
+                for col_name, dtype in df.dtypes.items():
+                    sql_dtype = dtype_mapping.get(str(dtype), 'TEXT')
+                    columns_definition.append(f'"{col_name}" {sql_dtype}')
+
+                create_table_statement = text(f'CREATE TABLE IF NOT EXISTS {table_name_with_schema} ({", ".join(columns_definition)})')
+                conn.execute(create_table_statement)
+                print(f"Created table if not exists: {table_name_with_schema}")
+
+                print(f"Truncating table: {table_name_with_schema}")
+                conn.execute(text(f'TRUNCATE TABLE {table_name_with_schema}'))
+                first_file_processed = True
+
+            try:
+                df.to_sql(table_name, conn, schema=TARGET_SCHEMA, if_exists="append", index=False)
+                print(f"Loaded {len(df)} records from {filename} into {table_name_with_schema}.")
+            except Exception as e:
+                print(f"Failed to load data from {filename} into {table_name_with_schema}: {e}")
